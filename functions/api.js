68ed6bde84fc45f00f6dbf2d7efa8f6ab9fd0d10
Akshith Marolie <akshith.marolie@acquia.com>
2025-12-03 21:28:52 +0530

    added functions.

---
+++ functions/api.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+
+
+const app = express();
+app.use(cors({
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST'],
+}));
+
+app.get('/hello', (req,res) => {
+  res.send('Hello World from serverless express!');
+})
+
+// jira proxy route
+app.post('/jira', async (req, res) => {
+  const jiraUrl = 'https://acquiaps.atlassian.net/rest/api/3/issue';
+  const JIRA_EMAIL = process.env.JIRA_EMAIL;
+  const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN;
+  const auth = Buffer.from(`${JIRA_EMAIL}:${JIRA_API_TOKEN}`).toString('base64');
+
+  if(!req.body) {
+    return res.status(400).json({ error: 'Request body is required' });
+  }
+  try {
+    const response = await axios.post(jiraUrl, req.body, {
+      headers: {
+        Authorization: `Basic ${auth}`,
+        Accept: 'application/json',
+      },
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error('Jira API error:', error.response?.data || error.message);
+    res.status(500).json({ error: 'Jira API call failed', details: error.response?.data || error.message });
+  }
+});
+
+
+
+module.exports.handler = serverless(app);
